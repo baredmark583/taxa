db4c5186c0d1509e6b75f6a8c6131d1e2e0a782d
baredmark583 <baredmark583@gmail.com>
2025-08-27 11:34:30 +0300

    Initial setup for Render

--- src/components/ProfileView.tsx
+++ src/components/ProfileView.tsx
@@ -50,13 +50,13 @@ const ProfileView: React.FC<ProfileViewProps> = ({ ads, viewAdDetails, navigateT
               onClick={() => alert('Coming soon!')} // navigateTo('favorites')
               label="Обране"
               disabled={true}
-              icon={<svg xmlns="http://www.w.org/2000/svg" className="h-6 w-6 text-tg-hint" fill="none" viewBox="0 0 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 016.364 0L12 7.636l1.318-1.318a4.5 4.5 0 016.364 6.364L12 20.364l-7.682-7.682a4.5 4.5 0 010-6.364z" /></svg>}
+              icon={<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-tg-hint" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M4.318 6.318a4.5 4.5 0 016.364 0L12 7.636l1.318-1.318a4.5 4.5 0 016.364 6.364L12 20.364l-7.682-7.682a4.5 4.5 0 010-6.364z" /></svg>}
           />
           <ProfileButton
               onClick={() => alert('Coming soon!')} // navigateTo('savedSearches')
               label="Збережені пошуки"
               disabled={true}
-              icon={<svg xmlns="http://www.w.org/2000/svg" className="h-6 w-6 text-tg-hint" fill="none" viewBox="0 0 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg>}
+              icon={<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 text-tg-hint" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" /></svg>}
           />
            {currentUser.role === 'ADMIN' && (
              <ProfileButton
